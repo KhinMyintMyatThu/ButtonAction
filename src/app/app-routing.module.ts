9c2ac03f9ddd249b1bb907099dd6b510a8651e1b
KhinMyintMyatThu <kmmt96@gmail.com>
2019-03-13 17:05:30 +0630

    Linking pages via buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'about-us', loadChildren: './about-us/about-us.module#AboutUsPageModule' },
+  { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
 ];
 
 @NgModule({
